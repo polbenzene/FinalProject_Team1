0cc5a713b529554f272082fcaa79644a699c7324
polbenzene <114271551+polbenzene@users.noreply.github.com>
2022-11-26 22:05:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask,render_template,Response
+from flask import Flask,render_template,Response,session,request,redirect,g,url_for
 import cv2
 import pickle
+import os
 
 #counters for parking_availability
 counter = 0 
 occupied = 0
 
 app=Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        session.pop('user', None)
+
+        if request.form['password'] == 'password':
+            session['user'] = request.form['username']
+            return redirect(url_for('protected'))
+
+    return render_template('LoginPage.html')
+
+@app.route('/protected')
+def protected():
+    if g.user:
+        return render_template('index.html',user=session['user'])
+    return redirect(url_for('protected'))
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user' in session:
+        g.user = session['user']
 
 #video source
 cap = cv2.VideoCapture('carpark.mp4')
@@ -79,19 +105,24 @@ def generate_frames():
 
         yield(b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_new + b'\r\n')
+                   
+#website 
+#@app.route('/')
+#def login():
+ #   return render_template('LoginPage.html', )
 
-@app.route('/')
-def index():
-    return render_template('index.html', )
-
+#processed parking lot video
 @app.route('/video')
 def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#available space count
 @app.route('/count')
 def count():
     p = counter
     return (str(p))
 
+#occupied space count
 @app.route('/occupied')
 def occcupied():
     o = occupied
@@ -100,14 +131,4 @@ def occcupied():
 if __name__=="__main_":
     app.run(debug=True)
 
-       # w, h = 220, 60
-      #  cv2.rectangle(frame_new, (0, 0), (w, h), (255, 0, 255), -1)
-      #  cv2.putText(frame_new, f"{counter}/{len(park_positions)}", (int(w / 10), int(h * 3 / 4)), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-      #  cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-      #  cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-       # cv2.imshow('frame', frame_new)
-        # cv2.imshow('image_blur', img_blur)
-        # cv2.imshow('image_thresh', img_thresh)
-
+       
